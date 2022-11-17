dda9481623ac6390c40376c8073fdf8adb48b9c1
Rusan Maharjan <rusanmhz098@gmail.com>
2022-11-17 15:29:24 +0545

    commit

---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import {Routes, Route, Navigate } from 'react-router-dom';
+import '../../assets/admin/css/styles.css';
+import '../../assets/admin/js/scripts';
+import Footer from "./Footer";
+
+import Navbar from './Navbar'
+import Sidebar from "./Sidebar";
+
+import routes from "../../routes/routes";
+
+const MasterLayout = () => {
+    return (
+        <div className="sb-nav-fixed">
+            <Navbar />
+
+            <div id="layoutSidenav">
+                <div id="layoutSidenav_nav">
+                    <Sidebar />
+                </div>
+
+                <div id="layoutSidenav_content">
+                    <main>
+                        <Routes>
+                            {routes.map((route, idx) => {
+                                return (
+                                    route.component && (
+                                        <Route 
+                                            key={idx}
+                                            path={route.path}
+                                            exact={route.exact}
+                                            name={route.name}
+                                            render={(props) => (
+                                                <route.component {...props} />
+                                            )}
+                                        />
+                                    )
+                                )
+                            })}
+                            <Route path="/admin" element={<Navigate to="/admin/dashboard" />}/>
+                            
+                        </Routes>
+
+                    </main>
+
+                    <Footer/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MasterLayout;
